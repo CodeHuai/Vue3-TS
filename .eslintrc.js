c8e9ca898aae38118531052096deb7c0b82e0daf
andbreak <1622170503@qq.com>
2024-01-08 22:29:05 +0800

    配置

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  // 可在rules自行添加规则
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false, // 句尾分号
+        useTabs: false, // 制表符缩进
+        singleQuote: true, // 使用 ''
+        trailingComma: 'es5', // 使用ES5 中有效的尾随逗号
+        endOfLine: 'lf', // 非空行下一行换行
+        tabWidth: 2 // 缩进
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        mocha: true
+      }
+    }
+  ]
+}
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+//此处的规则供参考，其中多半其实都是默认值，可以根据个人习惯改写
+module.exports = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: false
+}
