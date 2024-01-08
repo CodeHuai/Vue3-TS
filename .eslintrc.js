71aedc2d6f5c50d3998b9862b306a8b74fa9e621
CodeHuai <1622170503@qq.com>
2024-01-08 23:23:14 +0800

    处理 pretter 和 eslint 的冲突

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,36 +8,17 @@ module.exports = {
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
-    '@vue/prettier/@typescript-eslint'
+    '@vue/prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    sourceType: 'module'
   },
-  // 可在rules自行添加规则
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        semi: false, // 句尾分号
-        useTabs: false, // 制表符缩进
-        singleQuote: true, // 使用 ''
-        trailingComma: 'es5', // 使用ES5 中有效的尾随逗号
-        endOfLine: 'lf', // 非空行下一行换行
-        tabWidth: 2 // 缩进
-      }
-    ]
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        mocha: true
-      }
-    }
-  ]
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
 }
