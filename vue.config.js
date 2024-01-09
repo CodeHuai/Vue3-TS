d05dbe95ad3740a5f94ccc673d799edded0cea22
andbreak <1622170503@qq.com>
2024-01-09 20:04:47 +0800

    设置别名

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // 处理一个控制台警告
-  chainWebpack: (config) => {
-    config.plugin('define').tap((definitions) => {
-      Object.assign(definitions[0], {
-        __VUE_OPTIONS_API__: 'true',
-        __VUE_PROD_DEVTOOLS__: 'false',
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
-      })
-      return definitions
-    })
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        components: '@/components',
+        api: '@/api',
+        utils: '@/utils'
+      }
+    }
   }
 })
