cb8190aa47cc2779eacbd428e1ae8836bb218b8a
andbreak <1622170503@qq.com>
2024-01-11 21:33:22 +0800

    part_6

---
+++ src/components/nav-header/src/nav-header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>nav-header</div>
+</template>
+
+<style scoped lang="less">
+.nav-header {
+  .fold-menu {
+    font-size: 30px;
+    cursor: pointer;
+  }
+}
+</style>
