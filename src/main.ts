cc6bac17521ec770172e637825888e7945c3a772
andbreak <1622170503@qq.com>
2024-01-11 14:25:41 +0800

    删除请求

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,6 @@ import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import '@/assets/css/index'
 
-// 测试
-import network from '@/service'
-
-network.request({
-  url: '/login',
-  method: 'post',
-  data: {
-    "name": "coderwhy",
-    "password": "123456"
-  }
-}).then(res => {
-  console.log(res)
-})
-
 
 createApp(App).use(store).use(router).use(ElementPlus, {
   locale: zhCn
