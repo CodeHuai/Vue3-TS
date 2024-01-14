6205d5f5a05ca76b693d1eec4afe87c3aefa1017
andbreak <1622170503@qq.com>
2024-01-14 21:54:42 +0800

    part_9-3 role 配置

--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-
+  <div class="role">
+    <pageSearch :searchFormConfig="searchFormConfig"></pageSearch>
+    <PageContent :contentTableConfig="contentTableConfig" pageName="role"></PageContent>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import pageSearch from '@/components/page-search'
+import PageContent from '@/components/page-content'
+import { searchFormConfig } from './config/search.config'
+import { contentTableConfig } from './config/content.config'
 </script>
 
 <style scoped>
